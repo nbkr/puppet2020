b346fd9eeda3b34582b98bbab68b3bf46e5b1f6e
Ben Fleckenstein <bf@b1t.company>
2020-03-30 21:55:03 +0200

    Initial Import

---
+++ files/part4/testapp.py
@@ -0,0 +1,30 @@
+import falcon
+import MySQLdb
+import os
+
+class QuoteResource:
+
+    def on_get(self, req, resp):
+        """Handles GET requests"""
+
+        resp.content_type = 'text/html'
+
+        try:
+            db = MySQLdb.connect(host="change_me",
+                                 user="puppet2020",
+                                 passwd=os.environ['databasepassword'],
+                                 db="puppet2020")
+
+        except:
+            resp.body = '<html><head><title>Error</title><body><h1>Database not available!</h1></body></html>'
+            return
+
+        cur = db.cursor()
+        cur.execute("SELECT quote, author FROM quotes ORDER BY RAND() LIMIT 1")
+        data = cur.fetchone()
+
+        resp.body = '<html><head><title>Quote of the moment</title><body><h1>{}</h1>-- {}</body></html>'. \
+            format(data[0], data[1])
+
+app = falcon.API()
+app.add_route('/', QuoteResource())
